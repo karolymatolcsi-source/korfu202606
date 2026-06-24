47fbc86100d3e4086188ee4792be41fb56bbfe63
karolymatolcsi-source <karoly.matolcsi@gmail.com>
2026-06-24 22:58:08 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME='korfu-2026-v2';
+const CACHE_NAME='korfu-2026-final-v1';
 const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./terkep-korfu.png"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
